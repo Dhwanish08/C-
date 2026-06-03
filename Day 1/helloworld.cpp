8183ff92598fd2bdfce1e0759c804a9791126ed1
Dhwanish Desai <156456276+Dhwanish08@users.noreply.github.com>
2026-06-03 23:33:50 +0530

    1

---
+++ Day 1/helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  // Day 1
+
+  std::cout << "I am Dhwanish" << std::endl;
+  std::cout << "I am from Ahmedabad" << '\n';
+
+  int x;
+  x = 10;
+  int y = 10;
+  int sum = x + y;
+  std::cout << x <<" "<<y << std::endl;
+  std::cout << "Sum is " << sum << std::endl;
+  std::cout << x-y;
+  return 0;
+}
